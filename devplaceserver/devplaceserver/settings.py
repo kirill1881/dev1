1c18e36b673250fcc4c9f7827884170e7f3401b3
kirillsergeev <kirillovk317@gmail.com>
2023-07-19 12:56:27 +0300

    12

--- devplaceserver/devplaceserver/settings.py
+++ devplaceserver/devplaceserver/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cxc(lo50q7_$+au48g40yr(*)d1^+6ebit(8h_wt543sggmovg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -71,7 +71,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'devplaceserver.wsgi.application'
 
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
