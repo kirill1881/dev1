02c212ae80f1cdef47714742de69643fdc8b2e3f
kirillsergeev <kirillovk317@gmail.com>
2024-08-23 15:12:27 +0300

    231

--- devplaceserver/devapp/RekasoBot.py
+++ devplaceserver/devapp/RekasoBot.py
@@ -7,5 +7,6 @@ bot = telebot.TeleBot('7492253580:AAGPpfoKY_Wz7VwVa0soifF2mUTR9A3mXVA')
 
 def send_order(name, phone, tovar):
     bot.send_message("794818784", f'Имя: {name}\nТелефон: {phone}\nТовар: {tovar}\nВремя: {datetime.datetime.now()}')
+    bot.send_message("326183850", f'Имя: {name}\nТелефон: {phone}\nТовар: {tovar}\nВремя: {datetime.datetime.now()}')
 
 
